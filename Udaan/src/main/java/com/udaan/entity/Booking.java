9b3742d9ff77ec558148ecaa31e8f371a5983548
Pradeep <pradeeppunjabi70@gmail.com>
2023-02-06 16:49:22 +0530

    first

---
+++ Udaan/src/main/java/com/udaan/entity/Booking.java
@@ -0,0 +1,33 @@
+package com.udaan.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class Booking {
+	
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int bid;
+	
+	private int pnr;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@OneToMany(mappedBy = "booking",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Passenger> passenger;
+	
+}
---
+++ Udaan/src/main/java/com/udaan/entity/Passenger.java
@@ -0,0 +1,32 @@
+package com.udaan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Passenger {
+
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int pid;
+	private String pname;
+	private int pAge;
+	private String gender;
+	private String pmobile;
+	private String pemail;
+	private String pcountry;
+	
+	@ManyToOne
+	@JoinColumn(name = "booking_id")
+	private Booking booking;
+	
+	@ManyToOne
+	@JoinColumn(name = "flight_id")
+	private Flight flight;
+}
